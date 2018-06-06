602d5d6b721056074d00e38bb09018abdf69c82c
Mark <klynicol@users.noreply.github.com>
2018-06-05 18:26:17 -0700

    Update main.ts

--- client/main.ts
+++ client/main.ts
@@ -1,13 +1,9 @@
-import "angular2-meteor-polyfills";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
-
+import 'zone.js';
+import 'reflect-metadata';
 
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { enableProdMode } from "@angular/core";
 import { AppModule } from "./imports/app";
-
-import "./index.scss";
 
 enableProdMode();
 //starting point for the app.. bootstrapping what we need
